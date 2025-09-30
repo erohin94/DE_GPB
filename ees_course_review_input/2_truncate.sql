8efaaae9b239e1220889ae7ee040dd3e9f3de485
Evgeniy Erokhin <101813262+erohin94@users.noreply.github.com>
2025-09-30 16:52:02 +0300

    Add files via upload

---
+++ ees_course_review_input/2_truncate.sql
@@ -0,0 +1,10 @@
+/*Автор скрипта Ерохин Е.С.*/
+/*Выполнено в рамках задачи OPALAB-14848*/
+
+TRUNCATE TABLE IF EXISTS sbxm_hr.course_review_filtered_responses_tmp
+/
+TRUNCATE TABLE IF EXISTS sbxm_hr.course_review_recent_responses_tmp
+/
+TRUNCATE TABLE IF EXISTS sbxm_hr.course_review_final_responses_tmp
+/
+TRUNCATE TABLE IF EXISTS sbxm_hr.ees_course_review_input
